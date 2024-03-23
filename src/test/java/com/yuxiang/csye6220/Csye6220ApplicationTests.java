de8f092d32a1d5e881c2e3144bda02c678a2a0f5
Yuxiang Zhang <102555065+yxzhang97@users.noreply.github.com>
2024-03-23 14:51:06 -0400

    Yuxiang (#2)
    * modify .gitignore
    
    * commit
    
    * commit

---
+++ src/test/java/com/yuxiang/csye6220/Csye6220ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuxiang.csye6220;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Csye6220ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
