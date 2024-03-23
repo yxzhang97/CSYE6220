8b64982969573c134642431a0bb3a0b299f53134
quindex <zhang.yuxiang@foxmail.com>
2024-03-23 14:49:03 -0400

    commit

---
+++ src/main/java/com/yuxiang/csye6220/Csye6220Application.java
@@ -0,0 +1,13 @@
+package com.yuxiang.csye6220;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Csye6220Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Csye6220Application.class, args);
+	}
+
+}
