34d83477b4ca432bf21b1667e9450adf953e2fff
quindex <zhang.yuxiang@foxmail.com>
2024-04-10 12:42:00 -0400

    commit

--- src/main/java/com/yuxiang/csye6220/pojo/SellerEntity.java
+++ src/main/java/com/yuxiang/csye6220/pojo/SellerEntity.java
@@ -31,5 +31,71 @@ public class SellerEntity {
     @OneToMany(mappedBy = "seller")
     private List<ItemEntity> items;
 
+    @OneToMany
+    @JoinColumn(name = "fk_sellerEntity_id")
+    private List<OrderItemEntity> orderItems;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(Date dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public Date getDateLastModified() {
+        return dateLastModified;
+    }
+
+    public void setDateLastModified(Date dateLastModified) {
+        this.dateLastModified = dateLastModified;
+    }
+
+    public boolean isValid() {
+        return valid;
+    }
+
+    public void setValid(boolean valid) {
+        this.valid = valid;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<ItemEntity> getItems() {
+        return items;
+    }
+
+    public void setItems(List<ItemEntity> items) {
+        this.items = items;
+    }
+
+    public List<OrderItemEntity> getOrderItems() {
+        return orderItems;
+    }
+
+    public void setOrderItems(List<OrderItemEntity> orderItems) {
+        this.orderItems = orderItems;
+    }
 }
