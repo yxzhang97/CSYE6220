c2cd06ebf205085d99a60856109f776cd9e5057c
quindex <zhang.yuxiang@foxmail.com>
2024-04-15 15:41:27 -0400

    commit

---
+++ src/main/java/com/yuxiang/csye6220/controller/OrderController.java
@@ -0,0 +1,14 @@
+package com.yuxiang.csye6220.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class OrderController {
+
+    @Autowired
+    ApplicationContext applicationContext;
+
+
+}
